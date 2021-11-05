974aafe494a1449d6f2acb483fe992460ca399fe
Юрий <90134356+yuras11@users.noreply.github.com>
2021-11-05 17:09:54 +0300

    Update class.h

--- laba1.6/class.h
+++ laba1.6/class.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream>
-#include <numeric>
 using namespace std;
 class Fraction
 {
@@ -10,13 +9,12 @@ private:
 public:
     Fraction(int numerator,  int denominator);
     Fraction(const Fraction &copy);
-    void check_denominator(Fraction f);
-    void check_fraction_on_property(Fraction f);
+    int gcd (int a, int b);
     Fraction proper_Fraction(Fraction f);
     void printFraction();
     int getNumerator();
     int getDenominator();
-    Fraction getFraction(Fraction f);
+    static Fraction getFraction(Fraction f);
     static Fraction get_sum(Fraction f1, Fraction f2);
     static Fraction get_product(Fraction f1, Fraction f2);
     static Fraction get_quotient(Fraction f1, Fraction f2);
