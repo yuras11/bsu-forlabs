9899c909238d2c6e32874aa6e1deca07f7b2e2d6
Юрий <90134356+yuras11@users.noreply.github.com>
2021-11-23 22:13:45 +0300

    Update class.h

--- laba7/class.h
+++ laba7/class.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <iomanip>
 using namespace std;
 class Student
 {
@@ -15,47 +16,36 @@ public:
     Student(const Student &copy);
     friend ostream& operator<<(ostream& out, const Student &student);
     unsigned int GetID();
+    char* GetName();
     unsigned int GetCourse();
     unsigned int GetNumberOfGroup();
     unsigned int GetNumberOfCreditCard();
-    unsigned int **array_of_pointers;
-    virtual double fill_in_the_array();
+    void SetName(char* name_1);
+    void SetNumberOfGroup(unsigned int number_of_group_1);
+    void SetNumberOfCreditCard(unsigned int number_of_credit_card_1);
 };
 class StudentAfterFirstSession: public Student
 {
 protected:
-    unsigned int *marks[4];
+    unsigned int marks[4];
 public:
     StudentAfterFirstSession(char* name_1, unsigned int course_1, unsigned int number_of_group_1, unsigned int number_of_credit_card_1,
                              unsigned int first_mark, unsigned int second_mark, unsigned int third_mark, unsigned int forth_mark);
-    StudentAfterFirstSession(const StudentAfterFirstSession &copy, const StudentAfterFirstSession &copy1);
+    StudentAfterFirstSession(const Student &copy, const StudentAfterFirstSession &copy1);
     friend ostream& operator<<(ostream& out, const StudentAfterFirstSession &studentAfterFirstSession);
-    unsigned int GetFirstMark();
-    unsigned int GetSecondMark();
-    unsigned int GetThirdMark();
-    unsigned int GetForthMark();
     virtual double GetAveragePoint();
-    double fill_in_the_array() override;
 };
 class StudentAfterSecondSession: public StudentAfterFirstSession
 {
 protected:
-    unsigned int* marks_2[5];
+    unsigned int marks_2[5];
 public:
     StudentAfterSecondSession(char* name_1, unsigned int course_1, unsigned int number_of_group_1, unsigned int number_of_credit_card_1,
                               unsigned int first_mark, unsigned int second_mark, unsigned int third_mark, unsigned int forth_mark,
                               unsigned int first_mark_2, unsigned int second_mark_2, unsigned int third_mark_2, unsigned int forth_mark_2, unsigned int fifth_mark_2);
-    StudentAfterSecondSession(const StudentAfterFirstSession &copy, const StudentAfterFirstSession &copy1,
+    StudentAfterSecondSession(const Student &copy, const StudentAfterFirstSession &copy1,
                               const StudentAfterSecondSession &copy2);
     friend ostream& operator<<(ostream& out, const StudentAfterSecondSession &studentAfterSecondSession);
-    unsigned int GetFirstMark_2();
-    unsigned int GetSecondMark_2();
-    unsigned int GetThirdMark_2();
-    unsigned int GetForthMark_2();
-    unsigned int GetFifthMark_2();
     double GetAveragePoint() override;
-    double GetYearAveragePoint(StudentAfterFirstSession* &s, StudentAfterSecondSession* &s1);
-    double fill_in_the_array() override;
-    double GetAveragePointOfTheArray(Student* &s, StudentAfterFirstSession* &s1, StudentAfterSecondSession* &s2);
 };
 void solution();
