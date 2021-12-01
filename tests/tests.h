9d44868eefd62ab8c1608e6c5e51b4aedf28125c
Юрий <90134356+yuras11@users.noreply.github.com>
2021-12-01 19:42:53 +0300

    Create tests.h

---
+++ tests/tests.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <cassert>
+#include "functions.h"
+void test_1();
+void test_2();
+void test_3();
+void test_4();
+void test_5();
+void test_6();
+void test_7();
+void test_8();
+void test_9();
+void test_10();
+void run_tests();
