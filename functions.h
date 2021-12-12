f7ca49939fa8974456ae023e3b0c5c59ca065690
Юрий <90134356+yuras11@users.noreply.github.com>
2021-12-12 13:29:15 +0300

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "stack_on_array.h"
+bool isOperation(char temp);
+int Priority(char operation);
+double Calculations(double number1, double number2, char operation);
+double CalculatingTheExpression(string expression);
+void solution();
