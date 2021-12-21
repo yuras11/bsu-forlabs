ac391c6cf4e8ffab128d7b501b435204e2ccb10d
Юрий <90134356+yuras11@users.noreply.github.com>
2021-12-21 22:16:08 +0300

    Update stack_on_array.h

--- laba8/stack_on_array.h
+++ laba8/stack_on_array.h
@@ -10,15 +10,17 @@ protected:
 public:
     int size;
     T* st = new T[capacity];
-    Stack(): size(0), capacity(0){}
+    Stack(): size(0), capacity(5){}
     ~Stack() {delete[] st;}
     void Resize()
     {
         if (size + 1 > capacity)
         {
             T *new_st = new T[capacity + GrowsFactor];
-            for (size_t i = 0; i < size; ++i)
+            for (size_t i = 1; i <= size; ++i)
+            {
                 new_st[i] = st[i];
+            }
             capacity += GrowsFactor;
             delete[] st;
             st = new_st;
@@ -40,21 +42,24 @@ public:
     T pop()
     {
         if(IsEmpty())
-        throw runtime_error("Stack is empty");
-        T Value = st[size];
+        {
+            throw runtime_error("Stack is empty");
+        }
+        T data = st[size];
         --size;
-        return Value;
+        return data;
     }
     bool IsEmpty() {return !size;}
     void print(std::ostream &out)
     {
-        for(size_t i = size; i > 0; i--){
+        for(size_t i = size; i > 0; i--)
+        {
             out << st[i] << '\n';
         }
     }
     friend T operator<< (Stack<T> &stack, T data) {stack.push(data);}
     friend T operator>> (Stack<T> &stack, T data) {stack.pop();}
-     Stack& operator=(const Stack& stack){
+    Stack& operator=(const Stack& stack){
 
         this->size = stack.size;
         for (size_t i = 0; i <= size; i++)
